5f2a25da38334532737ec87cb09c27a358801a42
Dipika Valvi <dva@salecto.in>
2021-07-07 15:07:32 +0530

    Test commit 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dump('updated in master');
+        dump('updated in master again');
         return view('home');
     }
 }
